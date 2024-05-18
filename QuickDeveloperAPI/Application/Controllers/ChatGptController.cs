9fa9d90f2ec10d5b483963f081de708b8f891032
Matheus Guilherme <matheus.gsousa02@gmail.com>
2024-05-18 14:09:32 -0300

    First Deploy

---
+++ QuickDeveloperAPI/Application/Controllers/ChatGptController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services.Services;
+using Services.Services.Models;
+
+namespace Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatGptController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ChatGptController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost("CreateComplementation")]
+        public async Task<IActionResult> Create([FromBody] ComplementationRequest request)
+        {
+            var response = new CustomReturn<ComplementationResponse>();
+            response.Data = await new ChatGptServices(_configuration).CreateComplementation(request);
+
+            return Ok(response);
+        }
+    }
+}
---
+++ QuickDeveloperAPI/Services/Services/ChatGptServices.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using OpenAI;
+using OpenAI.Chat;
+using OpenAI.Models;
+using System.IO.Compression;
+using System.Text;
+
+namespace Services.Services
+{
+    public class ChatGptServices
+    {
+        private readonly IConfiguration _configuration;
+        private readonly UtilitiesServices _utilities;
+
+        public ChatGptServices(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _utilities = new UtilitiesServices();
+        }
+
+        public async Task<Models.ComplementationResponse> CreateComplementation(Models.ComplementationRequest request)
+        {
+            string apiKey = _configuration?.GetSection("Api-Key")?.Value ?? throw new Exception("Não foi possível recuperar a API Key da OpenAI na AppSettings. Por favor, verificar!");
+            using var openAIClient = new OpenAIClient(apiKey);
+
+            if (Guid.TryParse(request.SessionId, out Guid sessionId) == false)
+            {
+                sessionId = Guid.NewGuid();
+            }
+
+            string history = $"O QuickBot está auxiliando e tirando dúvidas do usuário {request.Name} para a prevenção de doenças infecciosas, respondendo ou o questionando sobre possíveis casos. O QuickBot não pode em qualquer momento fugir desse tema, independente da pergunta do usuário caso ela seja fora do escopo de doenças infecciosas\n";
+            if (!string.IsNullOrEmpty(request.History))
+            {
+                history = request.History;
+            }
+
+            string question;
+            string prompt = history + $"{request.Name}: " + request.Question + "\nQuickBot: ";
+
+            question = await GenerateQuestion(openAIClient, prompt, sessionId);
+
+            return new Models.ComplementationResponse
+            {
+                SessionId = sessionId,
+                Name = request.Name,
+                History = history + $"{request.Name}: " + request.Question + "\n",
+                Message = question,
+            };
+        }
+
+        private async Task<string> GenerateQuestion(OpenAIClient openAIClient, string prompt, Guid sessionId)
+        {
+            var messages = new List<Message>
+        {
+            new Message(Role.System, "You are a helpful assistant."),
+            new Message(Role.User, prompt)
+        };
+
+            var chatRequest = new ChatRequest(messages, Model.GPT3_5_Turbo, maxTokens: 500, user: sessionId.ToString());
+            var response = await openAIClient.ChatEndpoint.GetCompletionAsync(chatRequest);
+            var choice = response.FirstChoice;
+
+            _utilities.GerarLog(sessionId, chatRequest, response, "Chat", false);
+
+            return choice.Message;
+        }
+    }
+}
